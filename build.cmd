1bea886e6b6c68c1da14e0a5039eba0ed0112035
Claudemir <claudemir@bsd.com.br>
2021-09-01 16:10:50 -0300

    comandos para buildar

---
+++ build.cmd
@@ -0,0 +1,13 @@
+rem build  project
+
+mvnw clean package
+
+rem docker system prune --all --force --volumes
+
+rem docker build . -t claudemir1/desafio-soft:1.0
+
+rem docker image ls
+
+rem docker login
+
+rem docker push claudemir1/desafio-soft:1.0
